439d2564e1d775acb816fc3f6e28e41571097c1f
John Maguire <john@xap.ie>
2021-01-28 10:35:57 +0000

    add task recorder script and update gitignore and readme

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # TaskRecorder
 Small ruby script to record my daily tasks and compile my weekly notes
+
+To Run
+```
+ruby task-recorder.rb {command}
+```
+
+Available Commands:
+```
+done: "Add task to done for today",
+todo: "Add task to todo for tomorrow",
+notes: "Compile notes of completed tasks for the week incl. prior Friday as weekly meeting is Friday morning",
+standup: "Output notes to be pasted in dev-standup this morning",
+today: "Output notes for today",
+clean: "Remove files from all weeks but current one and prior Friday",
+help: "Display help notes for commands"
+```
+`done` and `todo` commands require a second param that is a description of the task, use Quotation marks to ensure this is taken as one string
+
+e.g.
+```
+ruby task-recorder.rb done "Long task that I did today"
+```
+or
+```
+ruby task-recorder.rb done "Long task that I will do tomorrow"
+```
+This will generate a task file for today like:
+```
+*Yesterday*
+>Long task that I did today
+*Today*
+>Long task that I will do tomorrow
+```
+At the end of the week you can run:
+```
+ruby task-recorder.rb notes
+```
+It will compile list of all tasks under the Yesterday header and compile them into a list of completed tasks for the week.
+
+At the strat of each day you can run
+```
+ruby task-recorder.rb standup
+```
+To see a list fo the tasks you finished yesterday and have scheduled to complete today
+
+At any point during the day you can run
+```
+ruby task-recorder.rb today
+```
+To see a list of the tasks you have completed today and what you have planned for tomorrow.
+
+
+Recommend adding this script as alias in bashrc so that you have easy access throughout console
+example bashrc entry
+```
+alias tr='ruby /path/to/repo/TaskRecorder/task-recorder.rb'
+```
+Then you can simply run
+````
+tr {command}
+```
---
+++ task-recorder.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/ruby
+require 'date'
+
+def add_new_item(note, done_item=true)
+  file_data = File.read("#{@root_dir}#{@todays_file}")
+  lines = file_data.split("\n")
+  position = -1
+  position = lines.index(@delim) if done_item
+  lines.insert(position, ">#{note}")
+
+  write_today(lines.join("\n"))
+end
+
+def write_today(content)
+  File.write("#{@root_dir}#{@todays_file}", content)
+end
+
+def output_file_content(filename=nil)
+  filename = @todays_file if filename.nil?
+  puts File.read("#{@root_dir}#{filename}")
+end
+
+def notes_for_week
+  today = Date.today
+  friday = prior_friday(today)
+  completed_tasks = []
+  today.downto(friday) do |day|
+    filename = "#{day.strftime('%F')}-tasks"
+    next unless File.exists?("#{@root_dir}#{filename}")
+    file_data = File.read("#{@root_dir}#{filename}")
+    delim = day.friday? ? "*Friday*" : "*Today*"
+    tasks = file_data.split(delim)[0].split("\n")
+    # get rid of header
+    tasks.shift
+    completed_tasks.concat(tasks)
+  end
+  output = completed_tasks.flatten.uniq.join("\n")
+  File.write("#{@root_dir}#{@weeks_file}", "*This Week*\n#{output}\n*Next Week*")
+end
+
+def clean
+  today = Date.today
+  friday = prior_friday(today)
+  save = today.downto(friday).map{|day| "#{day.strftime('%F')}-tasks"}
+  save << @weeks_file
+  Dir.foreach(@root_dir) do |filename|
+    next if filename == '.' or filename == '..' or (save.include? filename)
+    File.delete(filename)
+  end
+end
+
+def prior_friday(date)
+  days_before = (date.wday + 1) % 7 + 1
+  date.to_date - days_before
+end
+
+commands = %i{done todo notes standup today clean help}
+help_notes = {
+  done: "Add task to done for today",
+  todo: "Add task to todo for tomorrow",
+  notes: "Compile notes of completed tasks for the week incl. prior Friday as weekly meeting is Friday morning",
+  standup: "Output notes to be pasted in dev-standup this morning",
+  today: "Output notes for today",
+  clean: "Remove files from all weeks but this one and prior Friday",
+  help: "Display help notes for commands"
+}
+requires_note = %i{done todo}
+command = ARGV[0]&.to_sym
+raise "Unsupporterd command #{command}" unless commands.include? command
+
+note = ARGV[1]&.to_s
+raise "Note required for #{command}" if note.nil? && (requires_note.include? command)
+
+today = Date.today
+@root_dir = "#{__dir__}/task_files/"
+Dir.mkdir @root_dir unless File.exists?("#{@root_dir}")
+@todays_file = "#{today.strftime('%F')}-tasks"
+@weeks_file = "week-#{today.cweek}-#{today.year}-notes"
+@delim = today.friday? ? "*Friday*" : "*Today*"
+daily_template = "*Yesterday*\n#{@delim}\n"
+write_today(daily_template) unless File.exists?("#{@root_dir}#{@todays_file}")
+
+case command
+when :done
+  add_new_item(note)
+  output_file_content
+when :todo
+  add_new_item(note, false)
+  output_file_content
+when :notes
+  notes_for_week
+  puts "Compiled notes for week "
+  output_file_content(@weeks_file)
+when :standup
+  day = today.monday? ? prior_friday(today) : today.prev_day
+  standup_file = "#{day.strftime('%F')}-tasks"
+  output_file_content(standup_file)
+when :today
+  output_file_content
+when :clean
+  clean
+when :help
+  puts "Possible commands:\n"
+  help_notes.each do |command, info|
+    puts "#{command} => #{info}\n"
+  end
+end
